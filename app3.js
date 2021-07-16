2d35a4bea25c942e55486e293de4fc8b1d99d18c
JULIET STEVENSON <jjfstevenson@gmail.com>
2021-07-16 12:52:08 -0700

    updated lab06

---
+++ app3.js
@@ -0,0 +1,11 @@
+let wantTip = prompt("Would you like a crispy bottom insider's tip? Yes or No")
+
+if (wantTip == 'yes') {
+    document.write('<h2>Crispy Tip!</h2><p>When cooking your dumplings, make sure to make a "dumpling skirt" to ensure maximum crispyness. To do this, make a slurry of water and flour and add to the bottom of a pan while cooking your dumplings. Cover, and once you can see the golden brown, crispy, lace bottom, place a plate over the pan and flip over to remove the dumplings. Enjoy the crispyness!</p>')
+    document.write('<img src="https://redhousespice.com/wp-content/uploads/2020/05/Chicken-potsticker-with-crispy-skirt-2-scaled.jpg"/>')
+}
+
+else {
+    document.write("<h2> You'll never know how crispy a dumpling can get</h2>")
+    document.write('<img src="https://www.pressurecookingtoday.com/wp-content/uploads/2017/02/Asian-Steamed-Dumplings-Picture-720x405.jpg"/>')
+}
